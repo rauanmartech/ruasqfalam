ecda25d81b0cbe43abd107976880b79c0a04b9e3
rauanmartech <rauanrocha.martech@gmail.com>
2026-03-20 11:06:45 -0300

    Compatibilidade Vercel

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://pkqbdahzbmkifbnmghzc.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrcWJkYWh6Ym1raWZibm1naHpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5Njg5NTEsImV4cCI6MjA4OTU0NDk1MX0.-qQwdIUwdHcMGN7RMsdesVZea-ZZjiAg4_hoxx0-TUM';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
